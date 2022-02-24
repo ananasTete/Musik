47416ed6c7cbffccc4500ef1655b390fd4f6a3a2
李子航 <2045268468@qq.com>
2022-02-24 09:57:23 +0800

    detail-video页面请求mv数据

--- pages/detail-video/index.js
+++ pages/detail-video/index.js
@@ -1,17 +1,40 @@
 // pages/detail-video/index.js
+import {getMVURL, getMVDetail, getRelatedVideo} from '../../service/api_video'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    mvURLInfo: {},
+    mvDetail: {},
+    relatedVideo: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options.id);
+    const id = options.id;
+    this.getPageData(id)
+  },
+
+  getPageData: function(id) {
+    getMVURL(id).then(res => {
+      this.setData({
+        mvURLInfo: res.data
+      })
+    })
+    getMVDetail(id).then(res => {
+      this.setData({
+        mvDetail: res.data
+      })
+    })
+    getRelatedVideo(id).then(res => {
+      this.setData({
+        relatedVideo: res.data
+      })
+    })
   }
 })
--- service/api_video.js
+++ service/api_video.js
@@ -4,3 +4,24 @@ import request from './index'
 export function getTopMVs(offset, limit = 10) {
   return request.get('/top/mv', {offset, limit})
 }
+
+// 请求mv的播放地址
+export function getMVURL(id) {
+  return request.get("/mv/url", {
+    id
+  })
+}
+
+// 请求mv的详情
+export function getMVDetail(mvid) {
+  return request.get("/mv/detail", {
+    mvid
+  })
+}
+
+// 请求mv的关联视频
+export function getRelatedVideo(id) {
+  return request.get("/related/allvideo", {
+    id
+  })
+}
